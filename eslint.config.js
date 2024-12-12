831a6672f7f4e312c9676e49009d563f3f566065
Simon Sohneg <Simon_Sohneg@sluz.ch>
2024-12-12 14:25:54 +0100

    linter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,19 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.mocha } } },
-  pluginJs.configs.recommended,
-]
+  pluginJs.configs.recommended, // Standardempfohlene Regeln
+  {
+    rules: {
+      'no-unused-vars': 'error',   // Fehler bei nicht verwendeten Variablen
+      'prefer-const': 'error',     // Fehler, wenn let/var statt const verwendet wird
+      'eqeqeq': 'error',           // Fehler bei Verwendung von '==' statt '==='
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+      },
+    },
+  },
+];
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import chalk from 'chalk'
+import chalk from 'chalk';
 
-console.log(chalk.blue('It works'))
+let myUnusedVariable = 42; // Fehler: no-unused-vars und prefer-const
+
+var dynamicValue = "42"; // Dynamische Variable
+if (dynamicValue == "1") { // Kein konstanten Vergleich mehr
+    console.log('This is a weak equality check.');
+}
+
+console.log(chalk.blue('It works'));
